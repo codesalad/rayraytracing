155254155e25caed293f1e4502d9f98fbd6f251e
Maria Simidzioski <mariasimidzioski@hotmail.com>
2015-06-11 09:43:27 +0200

    changes.

--- source/raytracing.cpp
+++ source/raytracing.cpp
@@ -252,7 +252,16 @@ void ComputeAmbient()
 
 void ComputeDiffuse()
 {
+	Vec3Df normal, lightDir;
+	Vec3Df diffuse;
+	float dotProduct;
 
+//	normal = normalize(gl_NormalMatrix * gl_Normal);**/
+//lightDir = normalize(vec3(gl_LightSource[0].position));//
+	//NdotL = max(dot(normal, lightDir), 0.0);//
+	//diffuse = gl_FrontMaterial.diffuse * gl_LightSource[0].diffuse;
+	//gl_FrontColor = NdotL * diffuse;
+	//gl_Position = ftransform();
 }
 
 void ComputeSpecular()
